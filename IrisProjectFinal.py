910f813302ef372e0f890d00f7477533a969ddd9
Ereen <154412749+minicooperdaihatsu@users.noreply.github.com>
2023-12-23 04:07:38 +0800

    Update IrisProjectFinal.py

--- IrisProjectFinal.py
+++ IrisProjectFinal.py
@@ -34,7 +34,17 @@ def user_input_features():
 df = user_input_features()
     
     # Display the prediction
-    
+prediction = modelGaussianIris.predict(df)
+prediction_proba = modelGaussianIris.predict_proba(df)
+
+st.subheader('Class labels and their corresponding index number')
+st.write(Y.unique())
+
+st.subheader('Prediction')
+st.write(prediction)
+
+st.subheader('Prediction Probability')
+st.write(prediction_proba)    
 
 if __name__ == "__main__":
     main()

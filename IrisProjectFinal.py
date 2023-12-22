0d9c8d17605053a0693d36de57743fde65f4add7
Ereen <154412749+minicooperdaihatsu@users.noreply.github.com>
2023-12-23 04:17:32 +0800

    Update IrisProjectFinal.py

--- IrisProjectFinal.py
+++ IrisProjectFinal.py
@@ -1,8 +1,5 @@
 import streamlit as st
-import pandas as pd
-import seaborn as sns
 import pickle
-from sklearn.naive_bayes import GaussianNB
 
 # Load the pre-trained model
 with open('iris_model_gnb.pkl', 'rb') as file:
@@ -10,42 +7,21 @@ with open('iris_model_gnb.pkl', 'rb') as file:
 
 # Streamlit app
 def main():
-    st.title("Simple Iris Flower Prediction App")
-    st.write("This app predicts the **Iris flower** type!")
+    st.title("Iris Flower Prediction App (Gaussian Naive Bayes)")
 
-st.sidebar.header('User Input Parameters')
+    # Use st.sidebar for sliders in the sidebar
+    sepal_length = st.sidebar.slider("Sepal Length", 4.0, 8.0, 5.0)
+    sepal_width = st.sidebar.slider("Sepal Width", 2.0, 4.5, 3.0)
+    petal_length = st.sidebar.slider("Petal Length", 1.0, 7.0, 4.0)
+    petal_width = st.sidebar.slider("Petal Width", 0.1, 2.5, 1.0)
 
-    # Feature sliders for input values
-def user_input_features():
-    sepal_length = st.sidebar.slider('Sepal length', 4.3, 7.9, 5.4) 
-    sepal_width = st.sidebar.slider('Sepal width', 2.0, 4.4, 3.4)
-    petal_length = st.sidebar.slider('Petal length', 1.0, 6.9, 1.3)
-    petal_width = st.sidebar.slider('Petal width', 0.1, 2.5, 0.2)
-   
     # Predict the target class
-    data = {'sepal_length': sepal_length,
-            'sepal_width': sepal_width,
-            'petal_length': petal_length,
-            'petal_width': petal_width}                 
-    features = pd.DataFrame(data, index=[0])
-    return features
+    input_data = [[sepal_length, sepal_width, petal_length, petal_width]]
+    prediction = model.predict(input_data)[0]
 
-# Predict the target class
-df = user_input_features()
-    
     # Display the prediction
-iris_model_gnb = GaussianNB()
-modelGaussianIris.fit(X, Y)
-prediction = iris_model_gnb.predict(df)
-#prediction_proba = iris_model_gnb.predict_proba(df)
-
-st.subheader('Class labels and their corresponding index number')
-st.write(Y.unique())
-st.subheader('Prediction')
-st.write(prediction)
-
-#st.subheader('Prediction Probability')
-#st.write(prediction_proba)    
+    species_mapping = {0: 'Setosa', 1: 'Versicolor', 2: 'Virginica'}
+    st.write(f"Predicted Iris Species: {species_mapping[prediction]}")
 
 if __name__ == "__main__":
     main()

d0ff1212a80781ecd105984498e29c25ecb35027
Ereen <154412749+minicooperdaihatsu@users.noreply.github.com>
2023-12-23 04:02:22 +0800

    Update IrisProjectFinal.py

--- IrisProjectFinal.py
+++ IrisProjectFinal.py
@@ -21,14 +21,12 @@ def user_input_features():
     sepal_width = st.sidebar.slider('Sepal width', 2.0, 4.4, 3.4)
     petal_length = st.sidebar.slider('Petal length', 1.0, 6.9, 1.3)
     petal_width = st.sidebar.slider('Petal width', 0.1, 2.5, 0.2)
-   # predict the target class
     data = {'sepal_length': sepal_length,
             'sepal_width': sepal_width,
             'petal_length': petal_length,
             'petal_width': petal_width}                 
     features = pd.DataFrame(data, index=[0])
     return features
-# Call user defined function
 df = user_input_features()
     
     # Display the prediction

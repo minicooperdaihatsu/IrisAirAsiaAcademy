f0f0668fe5a08ccf170a10f0fc95b9ea6375ba70
Ereen <154412749+minicooperdaihatsu@users.noreply.github.com>
2023-12-23 04:10:25 +0800

    Update IrisProjectFinal.py

--- IrisProjectFinal.py
+++ IrisProjectFinal.py
@@ -34,8 +34,7 @@ def user_input_features():
 df = user_input_features()
     
     # Display the prediction
-prediction = iris_model_gnb.predict(df)
-prediction_proba = iris_model_gnb.predict_proba(df)
+
 
 st.subheader('Class labels and their corresponding index number')
 st.write(Y.unique())
